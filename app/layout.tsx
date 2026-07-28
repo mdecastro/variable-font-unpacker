053cec5d395fee79527029a5da0e2b45f491d398
Mariano de Castro <marianodecastro@gmail.com>
2026-07-28 17:03:24 -0300

    replace GTM

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import Script from "next/script";
 import { LanguageProvider } from "@/lib/i18n";
 import "./globals.css";
 
-const GTM_ID = "GTM-PVH3JJJD";
+const GA_ID = "G-TGLNSQ42C9";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -38,21 +38,16 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} ${recursive.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        {/* Google Tag Manager (noscript) */}
-        <noscript>
-          <iframe
-            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          />
-        </noscript>
-        {/* End Google Tag Manager (noscript) */}
-        <Script id="gtm" strategy="afterInteractive">{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','${GTM_ID}');`}</Script>
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+          strategy="afterInteractive"
+        />
+        <Script id="ga-gtag" strategy="afterInteractive">{`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GA_ID}');
+        `}</Script>
         <LanguageProvider>{children}</LanguageProvider>
       </body>
     </html>
