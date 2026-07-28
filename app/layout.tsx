82ec4957c9a05c0526012fe4eac4e01f7f818a4d
Mariano de Castro <marianodecastro@gmail.com>
2026-07-28 16:54:50 -0300

    Add GTM

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,10 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Recursive } from "next/font/google";
+import Script from "next/script";
 import { LanguageProvider } from "@/lib/i18n";
 import "./globals.css";
+
+const GTM_ID = "GTM-PVH3JJJD";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -35,6 +38,21 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} ${recursive.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+        <Script id="gtm" strategy="afterInteractive">{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${GTM_ID}');`}</Script>
         <LanguageProvider>{children}</LanguageProvider>
       </body>
     </html>
